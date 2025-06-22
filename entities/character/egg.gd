c72c7a6117304f415bd582f9346291b995f465ec
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-06-22 14:05:09 +0200

    particles and shit

--- entities/character/egg.gd
+++ entities/character/egg.gd
@@ -4,6 +4,7 @@ class_name Egg
 @export var character: RigidBody2D
 @export var egg_shape: CollisionPolygon2D
 @export var egg_sprite: AnimatedSprite2D
+@export var particle_emitter: Node2D
 
 @export var initial_speed: float = 300.0
 var speed: float
@@ -17,7 +18,6 @@ var time = 0.0
 @export var jump_strenght = 500.0
 
 func enter() -> void:
-	transitioned.emit(self, "chimken")
 	egg_shape.disabled = false
 	character.physics_material_override.friction = 1.0
 	speed = initial_speed
@@ -41,6 +41,7 @@ func physics_update(_delta: float) -> void:
 	character.apply_force(Vector2(speed, 0.0))
 	var current_speed: float = character.linear_velocity.length() 
 	if prev_speed - current_speed > crack_speed_threshold:
+		particle_emitter.emit_egg_shells()
 		transitioned.emit(self, "cracking")
 	prev_speed = current_speed
 	
